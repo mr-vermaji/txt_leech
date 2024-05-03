b4db92dc273b0f3c43d78aaca3f05ddb270bc1ae
mr-vermaji <167828828+mr-vermaji@users.noreply.github.com>
2024-05-03 20:50:25 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "28528007"
-api_hash = "38464da16c80310cabc8d13952419cf3"
-bot_token = "6979487512:AAGsN4-F7Jmd7f_fYoTQAR-xPXh9YzquOVY"
+api_id = "28947698"
+api_hash = "181af3baf73a37c24a429ca08e3f34f8"
+bot_token = "6846909009:AAHwRg2i5KLQnJ7GKJnyV1duCiRrKOpMJc0"
